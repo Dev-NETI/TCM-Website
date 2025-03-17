ccbc67e2d63ca0d69fd4eee604575c1b73cecdeb
Yujin <127990921+patrickeugenesacueza@users.noreply.github.com>
2025-03-17 13:03:09 +0800

    Setup
    Setup

---
+++ resources/views/components/heroSection.blade.php
@@ -0,0 +1,5 @@
+<div class="hero bg-gray-900 text-white text-center py-20">
+    <button class="mt-6 px-6 py-2 bg-blue-500 rounded-full hover:bg-blue-600">
+        Get Started
+    </button>
+</div>
