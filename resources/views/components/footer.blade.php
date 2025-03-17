ea14f59c75d799c4292f1017db10933407b90e9b
lemuelkso <156174740+lemuelkso@users.noreply.github.com>
2025-03-17 18:11:42 +0800

    add indicator in navigation

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,9 @@
-<footer class="relative text-white py-4 bg-linear-to-r from-blue-800 to-blue-900">
+<footer class="relative text-white px-4 py-12 bg-linear-to-r from-blue-800 to-blue-900 text-center ">
+
     <div>  
-        hello 
+
+        <p>Copyright © 2021 TDG Maritime</p> 
+
     </div>
+
 </footer>
