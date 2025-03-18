9cda334d887919953f7e865eee22d90b99bddafa
NOCDEPT <angeloperia3@gmail.com>
2025-03-18 13:05:50 +0800

    add logo in title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>TDG-MARITIME | @yield('title')</title>
-
+    <link rel="icon" type="image/png" href="{{ asset('images/tdg_logo.png') }}">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
